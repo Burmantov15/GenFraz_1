6d46b4182d4c042028dd26a73bae40de3bb88750
Burmantov15 <pupck1nvas1lij@yandex.ru>
2018-06-06 20:18:08 +0500

    tel_1

--- valid_2.php
+++ valid_2.php
@@ -5,7 +5,6 @@ if(!isset($_COOKIE['user_id'])) {
 	if(isset($_POST['vhod2'])) {
 		$user_username = mysqli_real_escape_string($dbc, trim($_POST['username']));
 		$user_password = mysqli_real_escape_string($dbc, trim($_POST['password']));
-
 		if(!empty($user_username) && !empty($user_password)) {
 			$query = "SELECT `user_id` , `username` FROM `signup` WHERE username = '$user_username' AND password = SHA('$user_password')";
 			$data = mysqli_query($dbc,$query);
@@ -13,7 +12,7 @@ if(!isset($_COOKIE['user_id'])) {
 				$row = mysqli_fetch_assoc($data);
 				setcookie('user_id', $row['user_id'], time() + (60*60*24*30));
 				setcookie('username', $row['username'], time() + (60*60*24*30));
-				echo "здраствуйте вы вошли в систему. Значит вход работает!";
+				echo 'здраствуйте вы вошли в систему. Значит вход работает!';
 			}
 			else {
 				echo 'Извините, вы должны ввести правильные имя пользователя и пароль';
